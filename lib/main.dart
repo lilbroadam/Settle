72b6380d9cc0f41ea05dcdc2321ed1a6cb0af04d
Rodrigo <restrella@outlook.com>
2020-12-23 12:32:37 -0500

    Fixed safe area for outside iphone notch and bar

--- lib/main.dart
+++ lib/main.dart
@@ -16,9 +16,7 @@ class SettleApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: SafeArea(
-        child: SettleHomePage(title: 'Settle')
-      ),
+      home: SettleHomePage(title: 'Settle'),
     );
   }
 }
@@ -77,7 +75,7 @@ class _SettleHomePageState extends State<SettleHomePage> {
   // fast, so that you can just rebuild anything that needs updating rather
   // than having to individually change instances of widgets.
   Widget build(BuildContext context) {
-    
+
     final createSettleButton = ElevatedButton(
         child: Text('Create a Settle'), onPressed: _createASettlePressed);
     final joinSettleButton = ElevatedButton(
@@ -86,55 +84,57 @@ class _SettleHomePageState extends State<SettleHomePage> {
     final miscButtonSize = 30.0;
 
     /**
-     * The home screen is made of an expanded stack that takes up the entire 
-     * screen and draws widgets in the center of the screen by default. The 
+     * The home screen is made of an expanded stack that takes up the entire
+     * screen and draws widgets in the center of the screen by default. The
      * Settle buttons are drawn in a column in the center of the stack/screen,
      * the info button is drawn in the bottom left of the stack/screen, and the
      * settings button is drawn in the bottom right of the stack/screen.
      */
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Expanded(
-            child: Stack(
-              alignment: Alignment.center,
-              children: <Widget>[
-                Column( // Settle buttons in the center
-                  mainAxisSize: MainAxisSize.min, // Size to only needed space
-                  children: <Widget>[
-                    Container(
-                      margin: settleButtonMargin,
-                      child: createSettleButton,
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Expanded(
+              child: Stack(
+                alignment: Alignment.center,
+                children: <Widget>[
+                  Column( // Settle buttons in the center
+                    mainAxisSize: MainAxisSize.min, // Size to only needed space
+                    children: <Widget>[
+                      Container(
+                        margin: settleButtonMargin,
+                        child: createSettleButton,
+                      ),
+                      Container(
+                        margin: settleButtonMargin,
+                        child: joinSettleButton,
+                      )
+                    ],
+                  ),
+                  Align( // Info button in the bottom left
+                    alignment: Alignment.bottomLeft,
+                    child: IconButton(
+                      icon: Icon(Icons.info),
+                      iconSize: miscButtonSize,
+                      tooltip: 'Information about Settle',
+                      onPressed: _informationPressed,
                     ),
-                    Container(
-                      margin: settleButtonMargin,
-                      child: joinSettleButton,
-                    )
-                  ],
-                ),
-                Align( // Info button in the bottom left
-                  alignment: Alignment.bottomLeft,
-                  child: IconButton(
-                    icon: Icon(Icons.info),
-                    iconSize: miscButtonSize,
-                    tooltip: 'Information about Settle',
-                    onPressed: _informationPressed,
                   ),
-                ),
-                Align( // Settings button in the bottom right
-                  alignment: Alignment.bottomRight,
-                  child: IconButton(
-                    icon: Icon(Icons.settings),
-                    iconSize: miscButtonSize,
-                    tooltip: 'Settle settings',
-                    onPressed: _settingsPressed,
+                  Align( // Settings button in the bottom right
+                    alignment: Alignment.bottomRight,
+                    child: IconButton(
+                      icon: Icon(Icons.settings),
+                      iconSize: miscButtonSize,
+                      tooltip: 'Settle settings',
+                      onPressed: _settingsPressed,
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
