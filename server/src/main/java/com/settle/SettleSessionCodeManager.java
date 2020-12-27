d0c7142cdb05b3c717c7ebdbc926c01f28f368cc
Ali Al-Adhami <aliraad@utexas.edu>
2020-12-27 21:26:35 +0000

    fixed code manager to increment properly

--- server/src/main/java/com/settle/SettleSessionCodeManager.java
+++ server/src/main/java/com/settle/SettleSessionCodeManager.java
@@ -18,16 +18,32 @@ public class SettleSessionCodeManager {
         return sessionCode;
     }
 
-    // TODO: make method iterate through supported codes properly
     private static synchronized String next() {
         String sessionCode = nextSessionCode;
 
         // Set the next session code
         StringBuffer stringBuffer = new StringBuffer();
         char[] chars = nextSessionCode.toCharArray();
-        for (char c : chars) {
-            c = (char) (c + 1);
-            stringBuffer.append(c);
+        int index = chars.length - 1;
+        boolean isChanged = false;
+        while(index >= 0) {
+            if (chars[index] != 'z') {
+                chars[index]++;
+                isChanged = true;
+                break;
+            } else {
+                chars[index] = 'a';
+                index--;
+            }
+        }
+        // check if reached max number of current code length
+        if (isChanged) {
+            for (char c : chars)
+                stringBuffer.append(c);
+        } else {
+            stringBuffer = new StringBuffer();
+            for(int i = 0; i < chars.length + 1; i++)
+                stringBuffer.append('a');
         }
         nextSessionCode = stringBuffer.toString();
 
