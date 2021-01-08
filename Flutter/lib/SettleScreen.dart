3a7cab2498e1d706eb3e9b8cca7cab23cd34ab43
lilbroadam <lilbroadam@gmail.com>
2021-01-08 04:18:55 -0600

    Add Flutter/lib/widgets/settle_card.dart

--- Flutter/lib/SettleScreen.dart
+++ Flutter/lib/SettleScreen.dart
@@ -1,64 +1,8 @@
-import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:tcard/tcard.dart';
 import 'package:groovin_widgets/groovin_widgets.dart';
+import 'widgets/settle_card.dart';
 import 'Settle.dart';
-
-List<Color> colors = [
-  Colors.blue,
-  Colors.yellow,
-  Colors.red,
-  Colors.orange,
-  Colors.pink,
-  Colors.amber,
-  Colors.cyan,
-  Colors.purple,
-  Colors.brown,
-  Colors.teal,
-];
-
-List<Color> pastelColors = [
-  Color(0xFFBFFCC6), // green
-  Color(0xFFFFCCF9), // pink
-  Color(0xFFC2EDFF), // blue
-  Color(0xFFD6D8FF), // purple
-  Color(0xFFFFCFC2), // orange
-];
-
-List<Widget> cards = List.generate(
-  colors.length,
-  (int index) {
-    return Container(
-        alignment: Alignment.center,
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.all(Radius.circular(5)),
-            color: colors[index],
-            image: DecorationImage(
-              fit: BoxFit.cover,
-              image: AssetImage('assets/adamoo.jpg'),
-            )),
-        child: Align(
-          alignment: Alignment.bottomLeft,
-          child: Container(
-              padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text('Card number $index',
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 20.0,
-                          fontWeight: FontWeight.w700)),
-                  Padding(padding: EdgeInsets.only(bottom: 8.0)),
-                  Text('A short description.',
-                      textAlign: TextAlign.start,
-                      style: TextStyle(color: Colors.white)),
-                ],
-              )),
-        ));
-  },
-);
 
 class SettleScreen extends StatefulWidget {
   final Settle settle;
@@ -80,25 +24,7 @@ class _SettleScreenState extends State<SettleScreen> {
     List<Widget> cards = new List();
   
     settle.options.forEach((option) {
-      cards.add(
-        new Container(
-          alignment: Alignment.center,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.all(Radius.circular(5)),
-            color: pastelColors[option.hashCode % pastelColors.length],
-          ),
-          child: Align(
-            alignment: Alignment.center,
-            child: Text('$option',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 20.0,
-                fontWeight: FontWeight.w700
-              ),
-            ),
-          )
-        ),
-      );
+      cards.add(SettleCard(option));
     });
 
     return cards;
